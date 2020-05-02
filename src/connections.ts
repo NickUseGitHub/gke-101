1a56a5c3d3cdf5a851cc4968628b284c82b0970f
Chaiwtun <chaiwtun@scg.com>
2020-05-02 18:07:51 +0700

    [master] create docker-compose

--- src/connections.ts
+++ src/connections.ts
@@ -1,17 +1,42 @@
 import { createConnection, Connection } from 'typeorm'
 
+async function connectToDB(tryCount: number): Promise<Connection | null> {
+  if (tryCount <= 0) return null
+
+  try {
+    const dbPort: number = Number(process.env.DB_PORT) || 5432
+    const connection: Connection = await createConnection({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: dbPort,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [`${__dirname}/entities/*.js`],
+    })
+    return connection
+  } catch (error) {
+    console.error(`:: Error :: ${tryCount} try to connect DB`, error)
+
+    await (function () {
+      return new Promise(function delay(resolve) {
+        const waitingTime = 3000
+        let countDown = waitingTime / 1000 - 1
+        const intervalId = setInterval(function () {
+          console.log(`try to connect in ${countDown}...`)
+          countDown--
+        }, 1000)
+
+        setTimeout(function () {
+          clearInterval(intervalId)
+          resolve()
+        }, waitingTime)
+      })
+    })()
+    return connectToDB(tryCount - 1)
+  }
+}
+
 export default async function initialConnection() {
-  const dbPort: number = Number(process.env.DB_PORT) || 5432
-
-  const connection: Connection = await createConnection({
-    type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: dbPort,
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    entities: [`${__dirname}/entities/*.js`],
-  })
-
-  return connection
+  return connectToDB(3)
 }
--- src/server.ts
+++ src/server.ts
@@ -8,19 +8,25 @@ const port = process.env.PORT || 3000
 /**
  * Start Express server.
  */
-initialConnection().then(function (connection: Connection) {
-  app.listen(port, () => {
-    if (connection.isConnected) {
-      console.log('[√] DB connected')
-    } else {
-      console.log('[X] DB connected')
-    }
+initialConnection()
+  .then(function (connection: Connection) {
+    app.listen(port, () => {
+      if (connection && connection.isConnected) {
+        console.log('[√] DB connected')
+      } else {
+        console.log('[X] DB no connected')
+        throw new Error('Cannot connect to DB')
+      }
 
-    console.log(
-      '  App is running at http://localhost:%d in %s mode',
-      process.env.PORT,
-      process.env.PROJECT_ENV,
-    )
-    console.log('  Press CTRL-C to stop\n')
+      console.log(
+        '  App is running at http://localhost:%d in %s mode',
+        process.env.PORT,
+        process.env.PROJECT_ENV,
+      )
+      console.log('  Press CTRL-C to stop\n')
+    })
   })
-})
+  .catch(function (error) {
+    console.error(error)
+    process.exit(1)
+  })
