20a2d11514d19cb52290a4328aecb0669fb8db1a
Chaiwtun <chaiwtun@scg.com>
2020-05-02 13:49:03 +0700

    [master] gm refactor code

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import "./configs";
-import app from "./app";
+import './configs'
+import app from './app'
 
 /**
  * Start Express server.
  */
 const server = app.listen(process.env.PORT, () => {
-    console.log(
-        "  App is running at http://localhost:%d in %s mode",
-        process.env.PORT,
-        process.env.PROJECT_ENV
-    );
-    console.log("  Press CTRL-C to stop\n");
-});
+  console.log(
+    '  App is running at http://localhost:%d in %s mode',
+    process.env.PORT,
+    process.env.PROJECT_ENV
+  )
+  console.log('  Press CTRL-C to stop\n')
+})
 
-export default server;
+export default server
